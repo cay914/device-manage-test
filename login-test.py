348011865e4f740452a118edd5166d60153e599a
tancaisheng <caisheng.tan@genlot.com>
2020-04-08 23:45:22 +0800

    添加登录测试

---
+++ login-test.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class Test1(unittest.TestCase):
+    def setUp(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox') # 这个配置很重要
+        self.driver = webdriver.Chrome(chrome_options=chrome_options, executable_path='/home/test/chromedriver')    # 如果没有把chromedriver加入
+        self.driver.implicitly_wait(3000)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_(self):
+        driver = self.driver
+        driver.get("http://192.168.3.31:8000/")
+        driver.find_element_by_id("account").click()
+        driver.find_element_by_id("account").clear()
+        driver.find_element_by_id("account").send_keys("admin")
+        driver.find_element_by_id("password").click()
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys("123456")
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
